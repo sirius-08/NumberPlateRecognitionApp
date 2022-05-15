c38d2a406b2456fefefc093470bdceea194f9b7a
sirius-08 <kalra.tanish13@gmail.com>
2022-05-15 16:58:23 +0530

    Initial Commit ; Added load image from gallery functionality

---
+++ NumberPlateRecognition/app/src/main/java/com/example/numberplaterecognition/LoadImageActivity.kt
@@ -0,0 +1,48 @@
+package com.example.numberplaterecognition
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+
+class LoadImageActivity : AppCompatActivity() {
+    lateinit var galleryButton : FloatingActionButton
+    lateinit var descriptionTextView : TextView
+    lateinit var imageView : ImageView
+    lateinit var imageUri : Uri
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load_image)
+
+        initViews()
+        galleryButton.setOnClickListener {
+            var intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(intent,100)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == 100) {
+            imageUri = data?.data!!
+            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, imageUri)
+            imageView.setImageBitmap(bitmap)
+            imageView.visibility = View.VISIBLE
+            descriptionTextView.visibility = View.GONE
+        }
+    }
+
+    fun initViews(){
+        galleryButton = findViewById(R.id.galleryButton) as FloatingActionButton
+        descriptionTextView = findViewById(R.id.descriptionTextView) as TextView
+        imageView = findViewById(R.id.imageView) as ImageView
+    }
+
+}
